b9a0999e0e740659745f4fab15fcb284062302d3
Don Becker <dbecker@exodus.psyon.org>
2020-03-21 19:43:53 -0500

    Put output in out/stl and out/stx to make things cleaner

--- build.sh
+++ build.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-mkdir -p stl;
-mkdir -p dxf;
+mkdir -p out/stl;
+mkdir -p out/dxf;
 
-grep '^module\s\+[a-z_]\+()\s\+{\s\+//\s\+make\s\+[0-9]\+$' crap-case.scad | awk '{print $2 $6}' | sed -e 's/()/ /' | while read -r MODULE COUNT; do
+grep '^module\s\+[a-z_]\+\(.*\)\s\+{\s\+//\s\+make\s\+[0-9]\+$' crap-case.scad | awk '{print $2 $6}' | sed -e 's/(.*)/ /' | while read -r MODULE COUNT; do
 	echo $MODULE;
 
-	if [ ! -f "stl/${MODULE}_x${COUNT}.stl" ]; then
+	if [ ! -f "out/stl/${MODULE}_x${COUNT}.stl" ]; then
 		echo -ne "use <crap-case.scad>\n$MODULE();" > temp.scad
-		openscad --render -D 'override_scale=25.4' -o stl/${MODULE}_x${COUNT}.stl temp.scad;
+		openscad --render -D 'override_scale=25.4' -o out/stl/${MODULE}_x${COUNT}.stl temp.scad;
 		rm temp.scad;
 	fi;
 
 	if [ `echo $MODULE | cut -d'_' -f 1` == "panel" ]; then
-		if [ ! -f "dxf/${MODULE}_x${COUNT}.dxf" ]; then
+		if [ ! -f "out/dxf/${MODULE}_x${COUNT}.dxf" ]; then
 			echo -ne "use <crap-case.scad>\nprojection() $MODULE();" > temp.scad
-			openscad --render -D 'override_scale=25.4' -o dxf/${MODULE}_x${COUNT}.dxf temp.scad;
+			openscad --render -D 'override_scale=25.4' -o out/dxf/${MODULE}_x${COUNT}.dxf temp.scad;
 			rm temp.scad;
 		fi;
 	fi;
